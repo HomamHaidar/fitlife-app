4bdbcb2b257bd146174331ca7c24402c23a9c689
Homam Haidar <homamhaidar68@gmail.com>
2025-12-03 17:44:23 +0300

    edit:Verification Email Customization

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
 
@@ -22,6 +24,15 @@ class AppServiceProvider extends ServiceProvider
     {
         Event::listen(function (\SocialiteProviders\Manager\SocialiteWasCalled $event) {
             $event->extendSocialite('google', \SocialiteProviders\Google\Provider::class);
+        });
+
+        VerifyEmail::toMailUsing(function (object $notifiable, string $url) {
+            return (new MailMessage)
+                ->subject('Welcome to FitLife! Please Verify Your Email')
+                ->greeting("Hello, {$notifiable->name}!")
+                ->line('One last step! Click the button below to confirm your email address and activate your FitLife account.')
+                ->action('Activate My Account', $url)
+                ->line('If you did not create an account with FitLife, you can safely ignore this email.');
         });
     }
 }
